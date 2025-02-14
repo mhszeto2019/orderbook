e1a65bab04a09d20b51f776d7523bd1a76839733
mhszeto2019 <brennanstmh@gmail.com>
2025-02-14 13:31:17 +0800

    update diaoyu left with delete case

--- app/strategies/algo_factory.py
+++ app/strategies/algo_factory.py
@@ -84,74 +84,156 @@ class AlgoFactory:
 
 
         
-    def add_or_update_algo(self, instance_id, algo_details):
-        """Add a new strategy or update an existing one."""
-        if instance_id in self.algos:
-            # logger.debug(algo_details)
-            # Update existing strategy
+    # def add_or_update_algo(self, instance_id, algo_details):
+    #     """Add a new strategy or update an existing one."""
+    #     if instance_id in self.algos:
+    #         # logger.debug(algo_details)
+    #         # Update existing strategy
             
-            shared_state = self.shared_states[instance_id]
+    #         shared_state = self.shared_states[instance_id]
 
-            logger.debug('UPDATING NEW STRAT')
+    #         logger.debug('UPDATING NEW STRAT')
 
-            json_data = algo_details.get('data','')
+    #         json_data = algo_details.get('data','')
 
-            # Update state in multiprocess
-            self.shared_states[instance_id]['lead_exchange'] = json_data['lead_exchange']
-            self.shared_states[instance_id]['lag_exchange'] = json_data['lag_exchange']
-            self.shared_states[instance_id]['spread'] = json_data['lead_exchange']
-            self.shared_states[instance_id]['qty'] = json_data['qty']
-            self.shared_states[instance_id]['ccy'] = json_data['ccy']
-            self.shared_states[instance_id]['instrument'] = json_data['instrument']
-            self.shared_states[instance_id]['contract_type'] = json_data['contract_type']
-            self.shared_states[instance_id]['state'] =  json_data['state']
+    #         # Update state in multiprocess
+    #         self.shared_states[instance_id]['lead_exchange'] = json_data['lead_exchange']
+    #         self.shared_states[instance_id]['lag_exchange'] = json_data['lag_exchange']
+    #         self.shared_states[instance_id]['spread'] = json_data['lead_exchange']
+    #         self.shared_states[instance_id]['qty'] = json_data['qty']
+    #         self.shared_states[instance_id]['ccy'] = json_data['ccy']
+    #         self.shared_states[instance_id]['instrument'] = json_data['instrument']
+    #         self.shared_states[instance_id]['contract_type'] = json_data['contract_type']
+    #         self.shared_states[instance_id]['state'] =  json_data['state']
      
-            strat_and_process = self.algos.get(instance_id)
-            strat = strat_and_process[0]
+    #         strat_and_process = self.algos.get(instance_id)
+    #         strat = strat_and_process[0]
 
-        else:
-            # Add a new strategy
-            logger.debug(f"Adding new strategy {instance_id}...")
-            logger.debug(algo_details)
+    #     else:
+    #         # Add a new strategy
+    #         logger.debug(f"Adding new strategy {instance_id}...")
+    #         logger.debug(algo_details)
 
-            # Create a shared state dictionary
-            # ['brennan_st', 'diaoyu', '123abc', 'okx', 'htx', '999', '1', 'na', 'swap', 'thisweek', False, 'e045967e-fbbc0636-e6d030e1-bewr5drtmh', '7d4bac9e-780e3558-de6db8f8-5a0df', 'a0de3940-5679-4939-957a-51c87a8502d9', 'FA44BCAAC3788C2AB4AFC77047930792', 'falconstead@Trading2024']
-            print('json_data',json_data)
-            row_dict  = {}
-            row_dict['username'] =  algo_details[0]
-            row_dict['algo_type'] = algo_details[1]
-            row_dict['algo_name'] = algo_details[2]
-            row_dict['lead_exchange'] = algo_details[3]
-            row_dict['lag_exchange'] = algo_details[4]
-            row_dict['spread']= algo_details[5]
-            row_dict['qty'] = algo_details[6]
-            row_dict['ccy'] = algo_details[7]
-            row_dict['instrument'] = algo_details[8]
-            row_dict['contract_type'] = algo_details[9]
-            row_dict['state'] = algo_details[10]    
-            # row_dict['trade_direction'] = algo_details[11]
-            row_dict['htx_apikey'] = algo_details[11]
-            row_dict['htx_secretkey'] = algo_details[12]
-            row_dict['okx_apikey'] = algo_details[13]
-            row_dict['okx_secretkey'] = algo_details[14]
-            row_dict['okx_passphrase'] = algo_details[15]
-            # row_dict[f'{row_dict['username']}_queue'] = self.queue
-            instance_id = f"{row_dict['username']}_{row_dict['algo_type']}_{row_dict['algo_name']}"
+    #         # Create a shared state dictionary
+    #         row_dict  = {}
+    #         row_dict['username'] =  algo_details[0]
+    #         row_dict['algo_type'] = algo_details[1]
+    #         row_dict['algo_name'] = algo_details[2]
+    #         row_dict['lead_exchange'] = algo_details[3]
+    #         row_dict['lag_exchange'] = algo_details[4]
+    #         row_dict['spread']= algo_details[5]
+    #         row_dict['qty'] = algo_details[6]
+    #         row_dict['ccy'] = algo_details[7]
+    #         row_dict['instrument'] = algo_details[8]
+    #         row_dict['contract_type'] = algo_details[9]
+    #         row_dict['state'] = algo_details[10]    
+    #         # row_dict['trade_direction'] = algo_details[11]
+    #         row_dict['htx_apikey'] = algo_details[11]
+    #         row_dict['htx_secretkey'] = algo_details[12]
+    #         row_dict['okx_apikey'] = algo_details[13]
+    #         row_dict['okx_secretkey'] = algo_details[14]
+    #         row_dict['okx_passphrase'] = algo_details[15]
+    #         # row_dict[f'{row_dict['username']}_queue'] = self.queue
+    #         instance_id = f"{row_dict['username']}_{row_dict['algo_type']}_{row_dict['algo_name']}"
 
-            self.shared_states[instance_id] = self.manager.dict(row_dict)
-            # logger.debug(self.shared_states)
-            # Create the new strategy instance (Diaoyu)
-            strat = Diaoyu(self.shared_states[instance_id], self.conn.cursor())
-            # Create a new process for the strategy
-            process = multiprocessing.Process(target=strat.start_clients)
-            # Store the strategy and process in the `algos` dictionary
-            self.algos[instance_id] = (strat, process)
-            # Store the shared state for the instance
-            # Start the process
-            process.start()
-            self.processes.append(process)
+    #         self.shared_states[instance_id] = self.manager.dict(row_dict)
+    #         # logger.debug(self.shared_states)
+    #         # Create the new strategy instance (Diaoyu)
+    #         logger.debug("CREATING NEW STRAT")
+    #         strat = Diaoyu(self.shared_states[instance_id], self.conn.cursor())
+    #         # strat = Diaoyu(self.shared_states[instance_id],psycopg2.connect(**DB_CONFIG).cursor())
+    #         logger.debug("CREATED NEW STRAT")
 
-            logger.debug(f"Added new strategy {instance_id} and started process.")
+    #         # Create a new process for the strategy
+    #         process = multiprocessing.Process(target=strat.start_clients)
+    #         # Store the strategy and process in the `algos` dictionary
+    #         self.algos[instance_id] = (strat, process)
+    #         # Store the shared state for the instance
+    #         # Start the process
+    #         process.start()
+    #         self.processes.append(process)
+
+    #         logger.debug(f"Added new strategy {instance_id} and started process.")
+
+    #         # p = multiprocessing.Process(target=strat.start_clients)
+    #         # self.algos[instance_id] = (strat, p)  # Update with the new process
+    #         # p.start()
+    #         # self.processes.append(p)
+
+    #     for p in self.processes:
+    #         p.join
+    def update_algo(self, instance_id, algo_details):
+        # logger.debug(algo_details)
+        # Update existing strategy
+        
+        shared_state = self.shared_states[instance_id]
+
+        logger.debug('UPDATING NEW STRAT')
+
+        json_data = algo_details.get('data','')
+
+        # Update state in multiprocess
+        self.shared_states[instance_id]['lead_exchange'] = json_data['lead_exchange']
+        self.shared_states[instance_id]['lag_exchange'] = json_data['lag_exchange']
+        self.shared_states[instance_id]['spread'] = json_data['lead_exchange']
+        self.shared_states[instance_id]['qty'] = json_data['qty']
+        self.shared_states[instance_id]['ccy'] = json_data['ccy']
+        self.shared_states[instance_id]['instrument'] = json_data['instrument']
+        self.shared_states[instance_id]['contract_type'] = json_data['contract_type']
+        self.shared_states[instance_id]['state'] =  json_data['state']
+
+        strat_and_process = self.algos.get(instance_id)
+        strat = strat_and_process[0]
+        # for p in self.processes:
+        #     p.join
+
+    def add_algo(self, instance_id, algo_details):
+        """Add a new strategy or update an existing one."""
+    
+        # Add a new strategy
+        logger.debug(f"Updating strategy {instance_id}...")
+        logger.debug(algo_details)
+
+        # Create a shared state dictionary
+        # print('json_data',json_data)
+        row_dict  = {}
+        row_dict['username'] =  algo_details[0]
+        row_dict['algo_type'] = algo_details[1]
+        row_dict['algo_name'] = algo_details[2]
+        row_dict['lead_exchange'] = algo_details[3]
+        row_dict['lag_exchange'] = algo_details[4]
+        row_dict['spread']= algo_details[5]
+        row_dict['qty'] = algo_details[6]
+        row_dict['ccy'] = algo_details[7]
+        row_dict['instrument'] = algo_details[8]
+        row_dict['contract_type'] = algo_details[9]
+        row_dict['state'] = algo_details[10]    
+        # row_dict['trade_direction'] = algo_details[11]
+        row_dict['htx_apikey'] = algo_details[11]
+        row_dict['htx_secretkey'] = algo_details[12]
+        row_dict['okx_apikey'] = algo_details[13]
+        row_dict['okx_secretkey'] = algo_details[14]
+        row_dict['okx_passphrase'] = algo_details[15]
+        # row_dict[f'{row_dict['username']}_queue'] = self.queue
+        instance_id = f"{row_dict['username']}_{row_dict['algo_type']}_{row_dict['algo_name']}"
+
+        self.shared_states[instance_id] = self.manager.dict(row_dict)
+        # logger.debug(self.shared_states)
+        # Create the new strategy instance (Diaoyu)
+        logger.debug("CREATING NEW STRAT")
+        strat = Diaoyu(self.shared_states[instance_id], self.conn.cursor())
+        # strat = Diaoyu(self.shared_states[instance_id],psycopg2.connect(**DB_CONFIG).cursor())
+        logger.debug("CREATED NEW STRAT")
+
+        # Create a new process for the strategy
+        process = multiprocessing.Process(target=strat.start_clients)
+        # Store the strategy and process in the `algos` dictionary
+        self.algos[instance_id] = (strat, process)
+        # Store the shared state for the instance
+        # Start the process
+        process.start()
+        self.processes.append(process)
+        logger.debug(f"Added new strategy {instance_id} and started process.")
 
         for p in self.processes:
             p.join
@@ -292,6 +374,7 @@ class DBListener(threading.Thread):
             while self.conn.notifies:
                 notify = self.conn.notifies.pop()
                 algo_details = json.loads(notify.payload)
+                logger.debug(f"ALGO DETAILS:{algo_details}")
                 # logger.debug(algo_details)
                 json_data = algo_details['data']
                 operation = algo_details['operation'] # db operations: update,insert...
@@ -324,7 +407,7 @@ class DBListener(threading.Thread):
                         FROM algo_dets ad left join api_credentials ac on ad.username = ac.username where ad.username= '{username}' and ad.algo_type ='{algo_type}' and algo_name='{algo_name}' group by ad.username,ad.algo_type,ad.algo_name,ad.lead_exchange,ad.lag_exchange,ad.spread,ad.qty,ad.ccy,ad.instrument,ad.contract_type,ad.state"""
                     )
                     new_algo_detail = cur.fetchone()
-                    self.factory.add_or_update_algo(instance_id,new_algo_detail)
+                    self.factory.add_algo(instance_id,new_algo_detail)
 
                 
                 # For updates
@@ -334,7 +417,7 @@ class DBListener(threading.Thread):
                     # logger.debug('UPDATE')
                     # logger.debug(self.factory.shared_states[instance_id])
                     # self.factory.shared_states[instance_id]['state'] = True
-                    self.factory.add_or_update_algo(instance_id,algo_details)
+                    self.factory.update_algo(instance_id,algo_details)
                     # logger.debug(self.factory.shared_states[instance_id])
 
                 else:
